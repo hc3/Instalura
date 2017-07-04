ca7d1f7f9b8b91d55b9f41b8b24d3a107064533b
hc3 <eliel.floyd@bol.com.br>
2017-07-03 23:16:00 -0300

    funcionalidade  liker 100%

--- instalura/src/components/Foto.js
+++ instalura/src/components/Foto.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router';
+import Pubsub from 'pubsub-js';
 
 class FotoHeader extends Component {
 
@@ -21,12 +22,33 @@ class FotoHeader extends Component {
 }
 
 class FotoInfo extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {likers : this.props.foto.likers};
+  }
+
+  componentWillMount() {
+    Pubsub.subscribe('atualiza-liker',(topico,infoLiker) => {
+     if(this.props.foto.id === infoLiker.fotoId) {
+       const possivelLiker =  this.state.likers.find(liker => liker.login === infoLiker.liker.login);
+       if(possivelLiker === undefined) {
+         const novosLikers = this.state.likers.concat(infoLiker.liker);
+         this.setState({likers:novosLikers});
+       } else {
+         const novosLikers = this.state.likers.filter(liker => liker.login != infoLiker.liker.login);
+         this.setState({likers:novosLikers}); 
+       }
+     }
+    });
+  }
+
   render() {
     return(
           <div className="foto-info">
             <div className="foto-info-likes">
             {
-              this.props.foto.likers.map(liker => {
+              this.state.likers.map(liker => {
                 return (<Link key={liker.login} to={`/timeline/${liker.login}`}>{liker.login},</Link>)
               })
             }
@@ -77,6 +99,7 @@ class FotoAtualizacoes extends Component {
       })
       .then(liker => {
         this.setState({likeada : !this.state.likeada});
+        Pubsub.publish('atualiza-liker',{fotoId:this.props.foto.id,liker});
       })
   }
 
